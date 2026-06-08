b54804a446b2d1f7beae83e62d32eaa26652e0d9
KRlSTOF <kristof.tick@gmail.com>
2026-06-08 14:33:17 +0000

    Fix asset paths for Capacitor APK

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gym-tracker/sw.js', { scope: '/gym-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
